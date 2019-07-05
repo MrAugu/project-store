fa25e9c4caeac405b72abb3aa919edb0c28846f8
Phem <phem@github.com>
2019-07-05 21:58:37 +0300

    so many changes

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const app = express();
 const path = require("path");
 const dataDir = path.resolve(`${process.cwd()}${path.sep}`);
 const config = require("./config.js");
-const templateDir = path.resolve(`${dataDir}${path.sep}views`);
+const templateDir = path.resolve(`${dataDir}${path.sep}`);
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const helmet = require("helmet");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
+const ejsLayouts = require("express-ejs-layouts");
 
 mongoose.connect(config.dbUrl, {
   useNewUrlParser: true
 });
 
 app.use("/assets", express.static(path.resolve(`${dataDir}${path.sep}assets`)));
-app.use("/public", express.static(path.join(__dirname, 'public'))); //public
+app.use("/public", express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.engine("html", ejs.renderFile);
 app.set("view engine", "ejs"); // changed html -> ejs
-app.set("views", path.join(__dirname, './app_server/views')); //views scope
+app.set('views', path.join(__dirname, '/app_server/views'));
 app.use(helmet());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(ejsLayouts);
 
 passport.serializeUser((user, done) => {
   done(null, user);
@@ -49,11 +51,12 @@ app.get("/login", (req, res) => routeFile("get", "login")(req, res, renderTempla
 app.post("/login", passport.authenticate("local", { failureRedirect: '/login' }), (req, res) => routeFile("post", "login")(req, res, renderTemplate));
 app.listen(config.port, null, null, () => console.log("Website is fully running."));
 
+
 function renderTemplate(res, req, template, data = {}) {
   const baseData = {
     user: req.isAuthenticated() ? req.user : null
   };
-  res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
+  res.render(path.resolve(`${templateDir}/app_server/views/${template}`), Object.assign(baseData, data));
 };
 
 function routeFile(method, fileName) {
